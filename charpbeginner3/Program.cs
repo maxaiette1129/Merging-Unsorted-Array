1312781d4458f25ea03f3a3d56945628f4c55e7e
maxaiette1129 <maxaiettec@gmail.com>
2023-04-27 23:10:29 +0800

    Add project files.

---
+++ charpbeginner3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace charpbeginner3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+           string merge = string.Join(" : ", cars);
+           Console.WriteLine($"The soring of inside the arary is {merge}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
